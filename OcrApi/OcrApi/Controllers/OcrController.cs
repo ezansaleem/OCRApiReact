d08b21f7fb490df478f99c1540dbab8bf6114c01
ezan <exansaleem@gmail.com>
2023-08-18 17:37:24 +0500

    .net and react project initially

---
+++ OcrApi/OcrApi/Controllers/OcrController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OcrApi.Services;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace OcrApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OcrController : ControllerBase
+    {
+        private readonly TesseractOcrService _ocrService;
+
+        public OcrController(TesseractOcrService ocrService)
+        {
+            _ocrService = ocrService;
+        }
+
+        [HttpPost("extract-text")]
+        public async Task<IActionResult> ExtractTextFromPdf([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded.");
+            }
+
+            using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            
+            var extractedText = _ocrService.ExtractTextFromPdfContent(memoryStream);
+            return Ok(new { ExtractedText = extractedText });
+        }
+    }
+}
---
+++ OcrApi/OcrApi/Services/TesseractOcrService.cs
@@ -0,0 +1,28 @@
+﻿using iTextSharp.text.pdf.parser;
+using System.Text;
+using Tesseract;
+using iTextSharp.text.pdf.parser;
+using iTextSharp.text.pdf;
+using System.Text;
+namespace OcrApi.Services
+{
+    public class TesseractOcrService
+    {
+        public string ExtractTextFromPdfContent(MemoryStream pdfContent)
+        {
+            
+            StringBuilder sb = new StringBuilder();
+            PdfReader reader = new PdfReader(pdfContent.ToArray());
+            {
+
+               for (int i = 1; i <= reader.NumberOfPages; i++)
+               {
+                        sb = sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                        
+               }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
